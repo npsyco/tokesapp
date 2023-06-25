160c1a83563dc56b2dd9f02fae5a81f032ff1d09
npsyco <npsyco@gmail.com>
2023-06-25 22:46:41 +0200

    asds

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var playButton = document.querySelector('.play-button');
     var audioPlayer = null;
     var isPlaying = false;
-    var audioUrl = 'YOUR_AZURECAST_URL'; // Replace with your Azurecast stream URL
+    var audioUrl = 'https://142.93.230.38:8000/radio.mp3'; // Replace with your Azurecast stream URL
   
     playButton.addEventListener('click', function() {
       if (isPlaying) {
